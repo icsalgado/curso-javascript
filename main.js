65c24bea974c691ce49e794e630738dec3aa0075
icsalgado <icsalgado@icloud.com>
2021-01-13 00:47:49 -0300

    funcções

--- main.js
+++ main.js
@@ -1,14 +1,6 @@
-var count = 0;
-while (count <=5){
-    console.log(count);
-    count++;
-};
+function soma(n1, n2){
+    return n1 + n2;
+}
 
-var count2;
-for(count2=0; count2 <=5; count2++ ){
-    alert(count2);
-};
+alert(soma(5, 10));
 
-var d = new Date();
-alert(d);
-alert(d.getMonth()+1);//sempre conta de 0, para o mes certo incremente 1
