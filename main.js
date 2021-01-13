12cb73788a1e4af378fc918f5ffee89886c8e6e0
icsalgado <icsalgado@icloud.com>
2021-01-13 01:17:25 -0300

    redirecionamento

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 function clicou(){
-    alert("obrigado por clicar");
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+    //console.log(document.getElementById("agradecimento"));
+}
+function redirecionar(){
+    window.open("https://www.google.com/");
 }
