d56af62bc9bc0d4b406c5dd1fab30af81b64af80
icsalgado <icsalgado@icloud.com>
2021-01-13 00:04:18 -0300

    console.log

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 var nome = "Iuri Salgado";//terminar em ;
-alert("Olá mundo javascript");
-alert("Bem vindo " + nome);//concatena com a variavel
 var num1 = 10;
 var num2 = 11;
-alert(num1 + num2); 
-
+/*
+alert("Olá mundo javascript");
+alert("Bem vindo " + nome);//concatena com a variavel
+alert(num1 + num2);//soma variavel 
+*/
+console.log(nome);
+console.log(num1 + num2);
