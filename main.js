ae5ac89b10f0611e4dac74f3662dd0f13f34d94a
icsalgado <icsalgado@icloud.com>
2021-01-13 00:22:16 -0300

    condicional if

--- main.js
+++ main.js
@@ -1,13 +1,7 @@
-var lista = ["maçã", "pera", "laranja"]; //lista
-var fruta = {nome:"maçã", cor:"vermelho"}; //dicionario, pode existir uma lista de dicionarios
-console.log(lista);
-console.log(lista[1]);
-lista.pop();
-console.log(lista);
-lista.push("uva");
-console.log(lista);
-console.log(lista.length);
-console.log(lista.reverse());
-console.log(lista.toString());
-console.log(lista.join(" - "));
-console.log(fruta);
+var idade = 18;
+
+if (idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+}
